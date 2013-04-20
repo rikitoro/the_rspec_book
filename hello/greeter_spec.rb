d072d6e3755d8dff181c793574824b623eaa5616
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-20 12:58:54 +0900

    first commit : Hello RSpec
    add greeter_spec.rb

---
+++ hello/greeter_spec.rb
@@ -0,0 +1,26 @@
+class RSpecGreeter
+	def greet
+		"Hello RSpec!"
+	end
+	
+	def bye
+		"sayonara"
+	end
+end
+
+describe "RSpec Greeter" do
+	it "should say 'Hello RSpec!' when it receives the greet() message" do
+		greeter = RSpecGreeter.new
+		greeting = greeter.greet
+		greeting.should == "Hello RSpec!"
+	end
+end
+
+
+describe "RSpec Byebye" do
+	it "should say 'Byebye' when it receives the bye() message" do
+		greeter = RSpecGreeter.new
+		greeting = greeter.bye
+		greeting.should == "Byebye"
+	end
+end
